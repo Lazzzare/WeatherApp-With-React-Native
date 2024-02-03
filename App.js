23831916f0870fdcdbca1adf04ad3137626e9e91
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-02-04 02:07:10 +0400

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 import { Feather } from "@expo/vector-icons";
-import CurrentWeather from "./src/components/CurrentWeather";
+import CurrentWeather from "./src/screens/CurrentWeather";
+import UpcomingWeather from "./src/screens/UpcomingWeather";
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
       <CurrentWeather />
+      {/* <UpcomingWeather /> */}
     </View>
   );
 }
