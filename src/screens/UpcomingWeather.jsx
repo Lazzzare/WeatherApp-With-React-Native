27fa9aab53d8202fb2d04fbd1067795484cf74d2
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-02-04 02:06:59 +0400

    Create UpcomingWeather.jsx

---
+++ src/screens/UpcomingWeather.jsx
@@ -0,0 +1,56 @@
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  FlatList,
+  View,
+  StatusBar,
+  ImageBackground,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import ListItem from "../components/ListItem";
+import data from "../data/data";
+import Empty from "../components/EmptyList";
+
+const UpcomingWeather = () => {
+  const renderItem = ({ item }) => {
+    return (
+      <ListItem
+        condition={item.weather[0].main}
+        dt_text={item.dt_text}
+        min={item.main.temp_min}
+        max={item.main.temp_max}
+      />
+    );
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Upcoming Weather</Text>
+      <ImageBackground
+        source={require("../../assets/weather_image.jpg")}
+        style={styles.image}
+      >
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.dt_text}
+          ListEmptyComponent={Empty}
+        />
+      </ImageBackground>
+    </SafeAreaView>
+  );
+};
+
+export default UpcomingWeather;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: "royalblue",
+  },
+
+  image: {
+    flex: 1,
+  },
+});
