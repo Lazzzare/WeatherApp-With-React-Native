27bdcba6ed1399ee9c583175ff04ab9566d022db
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-02-04 02:06:52 +0400

    Create data.js

---
+++ src/data/data.js
@@ -0,0 +1,38 @@
+export default data = [
+  {
+    dt_text: "2023-02-18 12:00:00",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [
+      {
+        main: "Clear",
+      },
+    ],
+  },
+  {
+    dt_text: "2024-03-11 05:00:00",
+    main: {
+      temp_max: 3.55,
+      temp_min: 1.55,
+    },
+    weather: [
+      {
+        main: "Rainly",
+      },
+    ],
+  },
+  {
+    dt_text: "2025-05-02 12:00:00",
+    main: {
+      temp_max: 24.55,
+      temp_min: 17.55,
+    },
+    weather: [
+      {
+        main: "Sun",
+      },
+    ],
+  },
+];
