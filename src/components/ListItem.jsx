8c509a53bca8c50c7f0d303939c0b06faaec100c
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-02-04 02:07:03 +0400

    Create ListItem.jsx

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const ListItem = ({ dt_text, min, max }) => {
+  return (
+    <View style={styles.item}>
+      <Feather name={"sun"} size={50} color={"white"} />
+      <Text style={styles.date}>{dt_text}</Text>
+      <Text style={styles.temp}>{min}</Text>
+      <Text style={styles.temp}>{max}</Text>
+    </View>
+  );
+};
+
+export default ListItem;
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderWidth: 5,
+    backgroundColor: "pink",
+  },
+  temp: {
+    color: "white",
+    fontSize: 20,
+  },
+  date: {
+    color: "white",
+    fontSize: 15,
+  },
+});
