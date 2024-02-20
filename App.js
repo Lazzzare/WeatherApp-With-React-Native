60ff39c9c1d0deafe918ec1a986d87b67488c33f
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-02-20 17:35:21 +0400

    fetching weather data and implement navigation between screens

--- App.js
+++ App.js
@@ -1,16 +1,69 @@
-import { StatusBar } from "expo-status-bar";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
-import { Feather } from "@expo/vector-icons";
-import CurrentWeather from "./src/screens/CurrentWeather";
-import UpcomingWeather from "./src/screens/UpcomingWeather";
-import City from "./src/screens/City";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./src/components/Tabs";
+import { ActivityIndicator } from "react-native";
+import { useState, useEffect } from "react";
+import { View, StyleSheet } from "react-native";
+import * as Location from "expo-location";
+import { WEATHER_API_KEY } from "@env";
 
 export default function App() {
+  const { container } = styles;
+  const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
+  const [weather, setWeather] = useState([]);
+
+  const fetchWeatherData = async () => {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+      );
+      const data = await res.json();
+      setWeather(data);
+      setLoading(false);
+    } catch (e) {
+      console.log(e);
+      setError("Could not fetch weather data");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        setError("permission to access location was denied");
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLat(location.coords.latitude);
+      setLon(location.coords.longitude);
+      await fetchWeatherData();
+    })();
+  }, [lat, lon]);
+
+  if (loading) {
+    return (
+      <View style={container}>
+        <ActivityIndicator size={"large"} color={"blue"} />
+      </View>
+    );
+  }
+
   return (
-    <View style={{ flex: 1 }}>
-      <CurrentWeather />
-      {/* <UpcomingWeather /> */}
-      {/* <City /> */}
-    </View>
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
